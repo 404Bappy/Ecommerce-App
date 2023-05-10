b471c3dad565139d9d833e7e9e8521b6c2796392
Bappy71 <19103076@iubat.edu>
2023-05-10 10:32:12 +0600

    Initial commit

--- client/src/Pages/PageNotFound.js
+++ client/src/Pages/PageNotFound.js
@@ -5,6 +5,14 @@ function PageNotFound() {
   return (
     <Layout>
       <h1>Page not Found</h1>
+
+      <>
+        <div className="container">
+          <h1>404</h1>
+          <p>Oops! The page you are looking for cannot be found.</p>
+          <a href="#">Go back to home</a>
+        </div>
+      </>
     </Layout>
   );
 }
--- client/src/Pages/Policy.js
+++ client/src/Pages/Policy.js
@@ -4,7 +4,24 @@ import Layout from "../Components/Layout/Layout";
 function Policy() {
   return (
     <Layout>
-      <h1>Here is the Policy</h1>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img className="rounded"
+            src="/images/Ecommerce-Privacy.jpg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+        </div>
+      </div>
     </Layout>
   );
 }
