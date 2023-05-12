8e04771082eadf1f46ae688b33aab44a472e6267
Bappy71 <19103076@iubat.edu>
2023-05-13 01:22:59 +0600

    Initial commit

--- client/src/Pages/Auth/Register.js
+++ client/src/Pages/Auth/Register.js
@@ -3,8 +3,36 @@ import Layout from "../../Components/Layout/Layout";
 
 const Register = () => {
   return (
-    <Layout title={'Register - Ecommerce App'}>
-      <h1>Register Page </h1>
+    <Layout title={"Register - Ecommerce App"}>
+      <div className="register">
+        <h1>fhdfsghdsghsdghgdfh</h1>
+        <form>
+          <div className="mb-3">
+            <label htmlFor="exampleInputName" className="form-label">
+              Name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputEmail1"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
     </Layout>
   );
 };
