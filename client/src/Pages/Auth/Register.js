d4c11aac5e126a34ec2d765986e7245f2f994f61
Bappy71 <19103076@iubat.edu>
2023-05-13 12:21:48 +0600

    Initial commit

--- client/src/Pages/Auth/Register.js
+++ client/src/Pages/Auth/Register.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import axios from "axios";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 import Layout from "../../Components/Layout/Layout";
 
 const Register = () => {
@@ -8,11 +10,29 @@ const Register = () => {
   const [password, setPassword] = useState("");
   const [phone, setPhone] = useState("");
   const [address, setAddress] = useState("");
+  const navigate = useNavigate();
 
-  const handleSumbit = (e) => {
+  const handleSumbit = async (e) => {
     e.preventDefault();
-    console.log(name, email, password, phone, address);
-    toast.success("Register Successfully");
+    try {
+      const res = await axios.post("/api/v1/auth/register", {
+        name,
+        email,
+        password,
+        phone,
+        address,
+      });
+
+      if (res.data.success) {
+        toast.success(res.data.message);
+        navigate("/login");
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something Went Wrong");
+    }
   };
 
   return (
