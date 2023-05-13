d4f7f7b1c388350b0305e948c3589223d1b0903b
Bappy71 <19103076@iubat.edu>
2023-05-13 09:44:27 +0600

    Initial commit

--- client/src/Pages/Auth/Register.js
+++ client/src/Pages/Auth/Register.js
@@ -7,54 +7,70 @@ const Register = () => {
   const [password, setPassword] = useState("");
   const [phone, setPhone] = useState("");
   const [address, setAddress] = useState("");
+
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    console.log(name, email, password, phone, address);
+  };
+
   return (
     <Layout title={"Register - Ecommerce App"}>
       <div className="register">
         <h1>fhdfsghdsghsdghgdfh</h1>
-        <form>
+        <form onSubmit={handleSumbit}>
           <div className="mb-3">
             <input
               type="text"
               value={name}
+              onChange={(e) => setName(e.target.value)}
               className="form-control"
               id="exampleInputEmail1"
               placeholder="Enter Your Name"
+              required
             />
           </div>
           <div className="mb-3">
             <input
               type="email"
               value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="form-control"
               id="exampleInputEmail1"
               placeholder="Enter Your Email"
+              required
             />
           </div>
           <div className="mb-3">
             <input
               type="password"
               value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="form-control"
               id="exampleInputPassword1"
               placeholder="Enter Your Password"
+              required
             />
           </div>
           <div className="mb-3">
             <input
               type="text"
               value={phone}
+              onChange={(e) => setPhone(e.target.value)}
               className="form-control"
               id="exampleInputEmail1"
               placeholder="Enter Your Phone Number"
+              required
             />
           </div>
           <div className="mb-3">
             <input
               type="text"
               value={address}
+              onChange={(e) => setAddress(e.target.value)}
               className="form-control"
               id="exampleInputEmail1"
               placeholder="Address"
+              required
             />
           </div>
 
--- client/src/Pages/Contact.js
+++ client/src/Pages/Contact.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Layout from "../Components/Layout/Layout";
 import {
-  BiBorderRadius,
+
   BiMailSend,
   BiPhoneCall,
   BiSupport,
