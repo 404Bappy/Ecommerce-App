7566e3964cf147dbfcaab442f9159dbe42c74f21
Bappy71 <19103076@iubat.edu>
2023-05-17 11:22:59 +0600

    initial Commits

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,15 @@ import Contact from "./Pages/Contact";
 import HomePage from "./Pages/HomePage";
 import PageNotFound from "./Pages/PageNotFound";
 import Policy from "./Pages/Policy";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import Login from "./Pages/Auth/Login";
+import Dashboard from "./Pages/User/Dashboard";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
