e2e0f8edad01bf9ad7c1da5037fed6f2b43405b9
Bappy71 <19103076@iubat.edu>
2023-05-18 10:15:18 +0600

    initial Commits

--- client/src/Pages/Auth/ForgotPassword.js
+++ client/src/Pages/Auth/ForgotPassword.js
@@ -1,10 +1,88 @@
-import React from "react";
-import Layout from "../../Components/Layout/Layout";
+import React, { useState } from "react";
 
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useAuth } from "../../context/auth";
+import Layout from "../../Components/Layout/Layout";
 const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [auth, setAuth] = useAuth();
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleSumbit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/api/v1/auth/login", {
+        email,
+        password,
+      });
+
+      if (res && res.data.success) {
+        toast.success(res.data && res.data.message);
+        setAuth({
+          ...auth,
+          user: res.data.user,
+          token: res.data.token,
+        });
+        localStorage.setItem("auth", JSON.stringify(res.data));
+        navigate(location.state || "/");
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something Went Wrong");
+    }
+  };
   return (
-    <Layout>
-      <h1>Forgot Password</h1>
+    <Layout title={"Forgot Password - Ecommerce App"}>
+      <div className="form-container" style={{ minHeight: "90vh" }}>
+        <form className="form" onSubmit={handleSumbit}>
+          <h4 className="title">LOGIN FORM</h4>
+
+          <div className="mb-3">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter Your Email"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Enter Your Password"
+              required
+            />
+          </div>
+
+          <div className="mb-3">
+            <button
+              type="submit"
+              className="btn btn-primary"
+              onClick={() => {
+                navigate("/forgot-password");
+              }}
+            >
+              Forgot Password
+            </button>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            LOGIN
+          </button>
+        </form>
+      </div>
     </Layout>
   );
 };
