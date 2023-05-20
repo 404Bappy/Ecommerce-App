34e5ba857a912fdef6a4c200b8f5567e61677b29
Bappy71 <19103076@iubat.edu>
2023-05-20 11:46:35 +0600

    initial Commits

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -3,12 +3,12 @@ import Layout from "../Components/Layout/Layout";
 import { useAuth } from "../context/auth";
 
 function HomePage() {
-  const [auth, setAuth] = useAuth();
+  const [auth, setAuth] = useAuth()
 
   return (
     <Layout title={"Best Offers"}>
       <h1>Home Page</h1>
-      <pre>{JSON.stringify(auth, null, 4)}</pre>
+      <pre>{JSON.stringify(auth,null,4)}</pre>
     </Layout>
   );
 }
