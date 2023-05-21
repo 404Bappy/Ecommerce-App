e8cd11e3230a55573c4d917768769b7b2093e95d
Bappy71 <19103076@iubat.edu>
2023-05-21 06:00:39 +0600

    initial Commits

--- client/src/Components/Layout/AdminMenu.js
+++ client/src/Components/Layout/AdminMenu.js
@@ -5,10 +5,10 @@ const AdminMenu = () => {
     <>
       <div className="text-center">
         <div className="list-group dashboard-menu">
-          <h4>Admin Panel</h4>
+          <h4 className="mt-4">Admin Panel</h4>
           <NavLink
             to="/dashboard/admin/create-category"
-            className="list-group-item list-group-item-action"
+            className="list-group-item list-group-item-action "
           >
             Create Category
           </NavLink>
--- client/src/Pages/Admin/CreateCategory.js
+++ client/src/Pages/Admin/CreateCategory.js
@@ -1,10 +1,20 @@
 import React from "react";
+import AdminMenu from "../../Components/Layout/AdminMenu";
 import Layout from "../../Components/Layout/Layout";
 
 const CreateCategory = () => {
   return (
-    <Layout>
-      <h1>Create CreateCategory</h1>
+    <Layout title={"Dashboard - Create Category"}>
+      <div className="container-fluid m-3 p-3">
+        <div className="row">
+          <div className="col-md-3">
+            <AdminMenu />
+          </div>
+          <div className="col-md-9 ">
+            <h1>Create CreateCategory</h1>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
