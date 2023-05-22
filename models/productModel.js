f54ad94c63e786e36d397f45842d3d3f0cf19105
Bappy71 <19103076@iubat.edu>
2023-05-22 07:30:25 +0600

    commit message

---
+++ models/productModel.js
@@ -0,0 +1,26 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: mongoose.objectId,
+    ref: "Category",
+  },
+});
+
+export default mongoose.model("Products", productSchema);
