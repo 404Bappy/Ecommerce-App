dcd9a9daff919aba423940afa58acfc284f1837c
Bappy71 <19103076@iubat.edu>
2023-06-15 01:48:26 +0600

    first commit

--- client/src/Pages/Admin/CreateProduct.js
+++ client/src/Pages/Admin/CreateProduct.js
@@ -48,11 +48,90 @@ const CreateProduct = () => {
                 bordered={false}
                 placeholder="SELECT A CATEGORY"
                 size="large"
-                showSearch className="form-select mb-3" onChange={(value) =>{setCategory(value)}}
+                showSearch
+                className="form-select mb-3"
+                onChange={(value) => {
+                  setCategory(value);
+                }}
               >
-                {categories?.map(c =>(
-                  <Option key={c.id} value={c.name}>{c.name}</Option>
+                {categories?.map((c) => (
+                  <Option key={c.id} value={c.name}>
+                    {c.name}
+                  </Option>
                 ))}
+              </Select>
+              <div className="mb-3">
+                <label className="btn btn-outline-secondary col-md-12">
+                  {photo ? photo.name : "Upload Photo"}
+                  <input
+                    type="file"
+                    name="photo"
+                    accept="image/*"
+                    onChange={(e) => setPhoto(e.target.files[0])}
+                    hidden
+                  />
+                </label>
+              </div>
+              <div className="mb-3">
+                {photo && (
+                  <div className="text-center">
+                    <img
+                      src={URL.createObjectURL(photo)}
+                      alt="product_photo"
+                      height={"200px"}
+                      className="img img-responsive"
+                    />
+                  </div>
+                )}
+              </div>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  value={name}
+                  placeholder="Write a name"
+                  className="form-control"
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <textarea
+                  type="text"
+                  value={description}
+                  placeholder="Write a description"
+                  className="form-control"
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="number"
+                  value={price}
+                  placeholder="Write a price"
+                  className="form-control"
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="number"
+                  value={quantity}
+                  placeholder="Write a quantity"
+                  className="form-control"
+                  onChange={(e) => setQuantity(e.target.value)}
+                />
+              </div>
+              <Select
+                bordered={false}
+                placeholder="Select-Shipping"
+                size="large"
+                showSearch
+                className="form-select mb-3"
+                onChange={(value) => {
+                  setShipping(value);
+                }}
+              >
+                <option value="0">No</option>
+                <option value="1">Yes</option>
               </Select>
             </div>
           </div>
