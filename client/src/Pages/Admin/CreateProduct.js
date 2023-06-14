2b2821d9e32bc42d2857d2d099438db643babd41
Bappy71 <19103076@iubat.edu>
2023-06-15 00:56:43 +0600

    first commit

--- client/src/Pages/Admin/CreateProduct.js
+++ client/src/Pages/Admin/CreateProduct.js
@@ -1,6 +1,11 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import AdminMenu from "../../Components/Layout/AdminMenu";
 import Layout from "../../Components/Layout/Layout";
+import { Select } from "antd";
+
+const { Option } = Select;
 
 const CreateProduct = () => {
   const [categories, setCategories] = useState([]);
@@ -11,6 +16,24 @@ const CreateProduct = () => {
   const [quantity, setQuantity] = useState("");
   const [photo, setPhoto] = useState("");
   const [shipping, setShipping] = useState("");
+
+  //Get All Category
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/category/getall-category");
+      if (data.success) {
+        setCategories(data.category);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something Went Wrong Getting Category");
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  }, []);
+
   return (
     <Layout title={"Dashboard - Create Product"}>
       <div className="container-fluid m-3 p-3">
@@ -20,6 +43,18 @@ const CreateProduct = () => {
           </div>
           <div className="col-md-9 ">
             <h1>Create Product</h1>
+            <div className="m-1 w-75">
+              <Select
+                bordered={false}
+                placeholder="SELECT A CATEGORY"
+                size="large"
+                showSearch className="form-select mb-3" onChange={(value) =>{setCategory(value)}}
+              >
+                {categories?.map(c =>(
+                  <Option key={c.id} value={c.name}>{c.name}</Option>
+                ))}
+              </Select>
+            </div>
           </div>
         </div>
       </div>
--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -1,14 +1,36 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import Layout from "../Components/Layout/Layout";
 import { useAuth } from "../context/auth";
 
 function HomePage() {
-  const [auth, setAuth] = useAuth()
+  const [auth, setAuth] = useAuth();
+  const [product, setProduct] = useState([]);
+  const [category, setCategory] = useState([]);
+
+  // GET_PRODUCTS
+  const getAllProducts = async () => {
+
+    try {
+      const {data} = await axios.get('/api/v1/product')
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <Layout title={"Best Offers"}>
-      <h1>Home Page</h1>
-      <pre>{JSON.stringify(auth,null,4)}</pre>
+    <Layout title={"All Productsc - Best Offers"}>
+      <div className="row mt-3">
+        <div className="col-md-3">
+          <h4 className="text-center">Filter by Category</h4>
+        </div>
+        <div className="col-md-9">
+          <h1 className="text-center">All Product</h1>
+          <div className="d-flex flex-wrap">
+            <h1>Products</h1>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
