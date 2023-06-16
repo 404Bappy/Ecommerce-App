09328d88cc86b78c80c13d422c376cfab0f3f23f
Bappy71 <19103076@iubat.edu>
2023-06-16 21:45:36 +0600

    first commit

--- client/src/Pages/Admin/Product.js
+++ client/src/Pages/Admin/Product.js
@@ -1,8 +1,27 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import AdminMenu from "../../Components/Layout/AdminMenu";
 import Layout from "../../Components/Layout/Layout";
 
 const Product = () => {
+  const [product, setProduct] = useState([]);
+
+  //GET ALL PRODUCT//
+  const getAllProducts = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/product/get-product");
+      setProduct(data.product);
+    } catch (error) {
+      console.log(error);
+      toast.error("Something Went Wrong");
+    }
+  };
+  //   LIFE Cycle METHOD //
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
   return (
     <Layout>
       <div className="row">
@@ -11,6 +30,9 @@ const Product = () => {
         </div>
         <div className="col-md-9">
           <h1 className="text-center">All Product List</h1>
+          {
+              
+          }
         </div>
       </div>
     </Layout>
