4f27d4eebb302ae06f2c64a3ca2e9b504b2f4dfa
Bappy71 <19103076@iubat.edu>
2023-06-17 20:18:26 +0600

    first commit

--- client/src/Pages/Admin/UpdateProduct.js
+++ client/src/Pages/Admin/UpdateProduct.js
@@ -420,7 +420,7 @@ const UpdateProduct = () => {
                   onChange={(value) => {
                     setShipping(value);
                   }}
-                  value={shipping ? "yes" : "No"}
+                  value={shipping ? "Yes" : "No"}
                 >
                   <Option value="0">No</Option>
                   <Option value="1">Yes</Option>
