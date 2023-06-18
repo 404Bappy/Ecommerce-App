86a39782a0a9d4b0011dd9aed3cdac31116b48b7
Bappy71 <19103076@iubat.edu>
2023-06-18 06:25:42 +0600

    first commit

--- client/src/Pages/Admin/Product.js
+++ client/src/Pages/Admin/Product.js
@@ -31,10 +31,10 @@ const Product = () => {
         </div>
         <div className="col-md-9 ">
           <h1 className="text-center">All Product List</h1>
-          <div className="d-flex">
+          <div className="d-flex flex-wrap">
             {product?.map((p) => (
               <Link key={p._id} to={`/dashboard/admin/product/${p.slug}`} className="product-link">
-                <div className="card" style={{ width: "18rem" }} >
+                <div className="card m-2" style={{ width: "18rem" }} >
                   <img src={`/api/v1/product/product-photo/${p._id}`} className="card-img-top" alt={p.name} />
                   <div className="card-body">
                     <h5 className="card-title">{p.name}</h5>
--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -1,5 +1,6 @@
+import { Button } from "antd";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../Components/Layout/Layout";
 import { useAuth } from "../context/auth";
 
@@ -10,13 +11,17 @@ function HomePage() {
 
   // GET_PRODUCTS
   const getAllProducts = async () => {
-
     try {
-      const {data} = await axios.get('/api/v1/product')
+      const { data } = await axios.get("/api/v1/product/get-product");
+      setProduct(data.product);
     } catch (error) {
       console.log(error);
     }
   };
+
+  useEffect(() => {
+    getAllProducts();
+  });
 
   return (
     <Layout title={"All Productsc - Best Offers"}>
@@ -27,7 +32,21 @@ function HomePage() {
         <div className="col-md-9">
           <h1 className="text-center">All Product</h1>
           <div className="d-flex flex-wrap">
-            <h1>Products</h1>
+            {product?.map((p) => (
+              <div className="card m-2" style={{ width: "18rem" }}>
+                <img
+                  src={`/api/v1/product/product-photo/${p._id}`}
+                  className="card-img-top"
+                  alt={p.name}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{p.name}</h5>
+                  <p className="card-text">{p.description}</p>
+                  <button class="btn btn-primary ms-1">More Details</button>
+                  <button class="btn btn-secondary ms-1">Add To Cart</button>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
