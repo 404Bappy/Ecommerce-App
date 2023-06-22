b2da98c9f7b7f82ad33740fc86cbd431cbbac932
Bappy71 <19103076@iubat.edu>
2023-06-23 00:09:17 +0600

    first commit

--- controller/productController.js
+++ controller/productController.js
@@ -246,6 +246,16 @@ export const productListController = async (req, res) => {
 //Search Product
 export const searchProductController = async (req, res) => {
   try {
+    const { keyword } = req.params;
+    const result = await productModel
+      .find({
+        $or: [
+          { name: { $regex: keyword, $options: "i" } },
+          { description: { $regex: keyword, $options: "i" } },
+        ],
+      })
+      .select("-photo");
+    res.json(result);
   } catch (error) {
     console.log(error);
     res.status(400).send({
